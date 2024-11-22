2f355361867448a656b93b62d2aec88fa7188ad1
Paulo Marques <paulomarques.rs1@gmail.com>
2024-11-22 10:12:55 -0300

    Add storage functionality for saving and loading data

--- storage.go
+++ storage.go
@@ -5,16 +5,19 @@ import (
 	"os"
 )
 
+// Represents a storage instance
 type Storage[T any] struct {
 	FileName string
 }
 
+// NewStorage creates a new storage instance
 func NewStorage[T any](fileName string) *Storage[T] {
 	return &Storage[T]{
 		FileName: fileName,
 	}
 }
 
+// Save marshals the data into JSON and writes it to the file
 func (s *Storage[T]) Save(data T) error {
 	fileData, err := json.MarshalIndent(data, "", "    ")
 
@@ -25,6 +28,7 @@ func (s *Storage[T]) Save(data T) error {
 	return os.WriteFile(s.FileName, fileData, 0644)
 }
 
+// Load reads the data from the file and unmarshals it into the provided data structure
 func (s *Storage[T]) Load(data *T) error {
 	fileData, err := os.ReadFile(s.FileName)
 
